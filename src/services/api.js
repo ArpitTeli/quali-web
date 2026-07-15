cb14a5451ac92d905ef8601b78a2dcf87252ea2c
ArpitTeli <arpitteli4@gmail.com>
2026-07-15 23:43:55 +0530

    chore: update LDS Apps Script URL

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ const PUSH_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwnHEf1fq1Mikib
 const MASTER_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyRKL4dz_F3nfWWW6APiDDKiVsiJ3uorX2MZr2g0t2oWZX8qhTx5-oknx0qDIs_PjrQ/exec'
 
 // LDS (Lead Distribution System) Apps Script
-const LDS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyuBqjTHlxK0Y1u_qTLUO6xxR4C3Al2VsvOMCnmwzjvInNB2GefYn4TccHy4e8mjTirxQ/exec'
+const LDS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxRh5kQFpJ8e8n9cDrjMCgwQb709uCrN2pXuEyavBSMN6hhrS2MfSB4kuGNJOI0BvZ-YA/exec'
 
 async function post(url, body) {
   const res = await fetch(url, {
